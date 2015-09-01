79b407f9a7777dcaa32934c382afd33355e68362
ediesky <ediesky@gmail.com>
2015-09-01 00:12:00 -0500

    Pico y Placa for Quito
    Pico y Placa for Quito

---
+++ predictor/predictor/predictorData.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace predictor
+{
+    public class predictorData
+    {
+        List<predictorItem> items;
+        public predictorData()
+        {
+            this.items = new List<predictorItem>();
+        }
+        public predictorData(List<predictorItem> items) 
+        {
+            this.items = items;
+        }
+        public void addPredictorItem( predictorItem item)
+        {
+            this.items.Add(item);
+        }
+        public List<predictorItem> getPredictorItem
+        {
+            get 
+            {
+                return this.items;
+            }
+        }
+    }
+    public class predictorItem
+    {
+        int dayNumber;
+        string day;
+        picoItem picos;
+        placaItem placas;
+        placaItem exceptions;
+        streetItem streets;
+        public predictorItem(int dayNumber, string day, picoItem picos, placaItem placas, placaItem exceptions, streetItem streets)
+        {
+            this.dayNumber = dayNumber;
+            this.day = day;
+            this.picos = picos;
+            this.placas = placas;
+            this.exceptions = exceptions;
+            this.streets = streets;
+        }
+        public int getDayNumber 
+        {
+            get 
+            {
+                return this.dayNumber;
+            }
+            set
+            {
+                this.dayNumber = value;
+            }
+        }
+        public string getDay
+        {
+            get
+            {
+                return this.day;
+            }
+            set
+            {
+                this.day = value;
+            }
+        }
+        public picoItem getPicos
+        {
+            get
+            {
+                return this.picos;
+            }
+            set
+            {
+                this.picos = value;
+            }
+        }
+        public placaItem getPlacas
+        {
+            get
+            {
+                return this.placas;
+            }
+            set
+            {
+                this.placas = value;
+            }
+        }
+        public streetItem getStreets
+        {
+            get
+            {
+                return this.streets;
+            }
+            set
+            {
+                this.streets = value;
+            }
+        }
+        public placaItem getExceptions
+        {
+            get
+            {
+                return this.exceptions;
+            }
+            set
+            {
+                this.exceptions = value;
+            }
+        }
+
+    }
+    public class picoItem
+    {
+        List<scheduleItem> schedules;
+        public picoItem()
+        {
+            this.schedules = new List<scheduleItem>();
+        }
+        public picoItem(List<scheduleItem> schedules)
+        {
+            this.schedules = schedules;
+        }
+        public List<scheduleItem> getSchedules 
+        {
+            get 
+            {
+                return this.schedules;
+            }
+        }
+        public void addSchedule( scheduleItem item) 
+        {
+            this.schedules.Add(item);
+        }
+        
+
+    }
+    public class placaItem
+    {
+        List<string> types;
+        public placaItem()
+        {
+            this.types = new List<string>();
+        }
+        public placaItem(List<string> types)
+        {
+            this.types = types;
+        }
+        public List<string> getTypes 
+        {
+            get 
+            {
+                return this.types;
+            }
+        }
+        public void addType( string type) 
+        {
+            this.types.Add(type);
+        }
+    }
+
+    public class scheduleItem
+    {
+        predictorTime begin;
+        predictorTime end;
+        public scheduleItem(string begin, string end)
+        {
+            this.begin = new predictorTime(begin);
+            this.end = new predictorTime(end);
+        }
+        public predictorTime getBegin 
+        {
+            get 
+            {
+                return this.begin;
+            }
+            set
+            {
+                this.begin = value;
+            }
+        }
+        public predictorTime getEnd
+        {
+            get
+            {
+                return this.end;
+            }
+            set
+            {
+                this.end = value;
+            }
+        }
+        
+    }
+    public class predictorTime
+    {
+        int hour;
+        int minute;
+        public predictorTime(int hour, int minute) 
+        {
+            this.hour = hour;
+            this.minute = minute;
+        }
+        public predictorTime(string time)
+        {
+            string[] arr = time.Split(':');
+            this.hour = int.Parse(arr[0]);
+            this.minute = int.Parse(arr[1]); ;
+        }
+        public int getHour
+        {
+            get
+            {
+                return this.hour;
+            }
+            set
+            {
+                this.hour = value;
+            }
+        }
+        public int getMinute
+        {
+            get
+            {
+                return this.minute;
+            }
+            set
+            {
+                this.minute = value;
+            }
+        }
+        public bool isAfter(predictorTime timeCompare) 
+        {
+            if (this.getHour > timeCompare.getHour) return true;
+            else if (this.getHour == timeCompare.getHour)
+            {
+                if (this.minute >= timeCompare.getMinute) return true;
+                else return false;
+            }
+            else return false;
+        }
+
+        public bool isBefore(predictorTime timeCompare)
+        {
+            if (this.getHour < timeCompare.getHour) return true;
+            else if (this.getHour == timeCompare.getHour)
+            {
+                if (this.minute <= timeCompare.getMinute) return true;
+                else return false;
+            }
+            else return false;
+        }
+
+
+    }
+    public class streetItem
+    {
+        List<string> streets;
+        public streetItem()
+        {
+            streets = new List<string>();
+        }
+        public streetItem(List<string> streets)
+        {
+            this.streets = streets;
+        }
+        public List<string> getStreets
+        {
+            get
+            {
+                return this.streets;
+            }
+        }
+        public void addStreet(string street)
+        {
+            streets.Add(street);
+        }
+
+    }
+    
+    
+}
---
+++ predictor/predictor/xmlParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace predictor
+{
+    public class xmlParser
+    {
+        public static predictorData readXml() 
+        {
+            predictorData dataList = new predictorData();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("data.xml");
+
+            XmlNode restrictionListNode = doc.SelectSingleNode("/restriction");
+            XmlNodeList detailNodeList = restrictionListNode.SelectNodes("detail");
+            
+            foreach (XmlNode node in detailNodeList)
+            {
+                string day = node.SelectSingleNode("day").InnerText;
+                int dayNumber = int.Parse(node.SelectSingleNode("dayNumber").InnerText);
+
+                picoItem pico = new picoItem();
+                XmlNode picoListNode = node.SelectSingleNode("pico");
+                XmlNodeList scheduleNodeList = picoListNode.SelectNodes("schedule");
+                foreach (XmlNode nodeSchedule in scheduleNodeList) 
+                {
+                    string begin = nodeSchedule.SelectSingleNode("begin").InnerText;
+                    string end = nodeSchedule.SelectSingleNode("end").InnerText;
+                    pico.addSchedule(new scheduleItem(begin, end));
+                }
+
+                placaItem placa = new placaItem();
+                XmlNode placaListNode = node.SelectSingleNode("placa");
+                XmlNodeList typeNodeList = placaListNode.SelectNodes("type");
+                foreach (XmlNode nodeType in typeNodeList)
+                {
+                    string type = nodeType.InnerText;
+                    placa.addType(type);
+                }
+
+                placaItem exceptions = new placaItem();
+                XmlNode exceptionsListNode = node.SelectSingleNode("exception");
+                XmlNodeList exceptionsNodeList = exceptionsListNode.SelectNodes("type");
+                foreach (XmlNode nodeType in exceptionsNodeList)
+                {
+                    string type = nodeType.InnerText;
+                    exceptions.addType(type);
+                }
+
+                streetItem streets = new streetItem();
+                XmlNode streetListNode = node.SelectSingleNode("street");
+                XmlNodeList streetsNodeList = streetListNode.SelectNodes("streetName");
+                foreach (XmlNode nodeStreet in streetsNodeList)
+                {
+                    string streetName = nodeStreet.InnerText;
+                    streets.addStreet(streetName);
+                }
+
+                predictorItem item = new predictorItem(dayNumber, day, pico, placa, exceptions, streets);
+                dataList.addPredictorItem(item);
+            }
+            return dataList;
+        }
+    }
+}
